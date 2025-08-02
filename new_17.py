1f0bc30047533f1c292599ee4dcef92a5337c95d
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2025-08-03 00:29:59 +0500

    Update new_17.py

--- new_17.py
+++ new_17.py
@@ -1,19 +1,19 @@
 # 17 new Massiv elementlarini avval barcha nol
 import random
 
-print(random.randint(1, 30))
+print(random.randint(1, 38))
 
-n = int(input("N = "))
-nums = []
-for i in range(n):
-    num = int(input(f"{i + 1} - "))
-    nums.append(num)
-
-for i in range(n):
-    for j in range(1, n):
-        if nums[j] == 0:
-            nums[j] = nums[j - 1]
-            nums[j - 1] = 0
-
-print(nums)
+# n = int(input("N = "))
+# nums = []
+# for i in range(n):
+#     num = int(input(f"{i + 1} - "))
+#     nums.append(num)
+#
+# for i in range(n):
+#     for j in range(1, n):
+#         if nums[j] == 0:
+#             nums[j] = nums[j - 1]
+#             nums[j - 1] = 0
+#
+# print(nums)
 
